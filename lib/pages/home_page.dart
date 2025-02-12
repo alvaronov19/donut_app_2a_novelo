eba0a2f82a0e152561c04613147b7fdd95276802
Alvaro Novelo <alvaronovelo19@gmail.com>
2025-02-12 09:04:02 -0600

    Main Text Added

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -10,6 +10,44 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          //Icono de la izquierda
+          leading: Icon(
+            Icons.menu,
+            color: Colors.grey[800],
+          ),
+          actions: [
+            Padding(
+              padding: const EdgeInsets.only(right: 24.0),
+              child: Icon(Icons.person),
+            )
+          ],
+        ),
+        body: const Column(
+          children: [
+            //Texxto "I want to eat"
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 36, vertical: 18),
+              child: Row(
+                children: [
+                  Text("I want to",
+                  style: TextStyle(fontSize: 32),),
+                  Text(
+                    "Eat", 
+                    style: TextStyle( 
+                    fontSize: 32,
+                    fontWeight: FontWeight.bold,
+                    decoration: TextDecoration.underline),
+                )
+              ],
+            ),
+          )
+          //Tab bar
+
+          //Carrito
+        ],
+        ));
   }
 }
